e0ee7a4e26e6f14073d58217bb89ff2e49c4099f
10NUMERO <florian.gozard@edu.devinci.fr>
2021-10-14 19:10:18 +0200

    FROM 26 LIGNES TO 7

--- Assets/Teams/Leviathan/SetShockwave.cs
+++ Assets/Teams/Leviathan/SetShockwave.cs
@@ -1,26 +1,7 @@
 using BehaviorDesigner.Runtime.Tasks;
-using BehaviorDesigner.Runtime;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Leviathan;
 
 public class SetShockwave : Action
 {
-    public SharedBool setShockwaveValue;
-    private LeviathanController leviathan;
-    private BehaviorTree tree;
-
-    public override void OnStart()
-    {
-        tree = gameObject.GetComponentInParent<BehaviorTree>();
-        leviathan = tree.GetComponentInParent<LeviathanController>();
-        setValue(setShockwaveValue.Value);
-    }
-
-    public void setValue(bool shockwaveValue)
-    {
-        leviathan.setShockwaveCondition(shockwaveValue);
-        tree.SetVariableValue("Shockwave", shockwaveValue);
-    }
+    public override void OnStart()=> LeviathanController.instance.setShockwaveCondition(true);
 }
