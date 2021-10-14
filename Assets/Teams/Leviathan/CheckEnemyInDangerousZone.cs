f8f7f0b6441d2ee0503445cb5cc44415316294f8
10NUMERO <florian.gozard@edu.devinci.fr>
2021-10-14 20:03:09 +0200

    namespace

--- Assets/Teams/Leviathan/CheckEnemyInDangerousZone.cs
+++ Assets/Teams/Leviathan/CheckEnemyInDangerousZone.cs
@@ -1,22 +1,22 @@
 using BehaviorDesigner.Runtime.Tasks;
 using BehaviorDesigner.Runtime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Leviathan;
 
-public class CheckEnemyInDangerousZone : Action
+namespace Leviathan
 {
-    public SharedFloat dangerousRange;
-
-    public override void OnStart()
+    public class CheckEnemyInDangerousZone : Action
     {
-        float enemyDist = Vector2.Distance(LeviathanController.instance._spaceship.Position, LeviathanController.instance._otherSpaceship.Position);
+        public SharedFloat dangerousRange;
 
-        if(enemyDist <= dangerousRange.Value)
-            LeviathanController.instance.tree.SetVariableValue("EnemyIsInDangerousRange", true);
-        else 
-            LeviathanController.instance.tree.SetVariableValue("EnemyIsInDangerousRange", false);
+        public override void OnStart()
+        {
+            float enemyDist = Vector2.Distance(LeviathanController.instance._spaceship.Position, LeviathanController.instance._otherSpaceship.Position);
 
+            if (enemyDist <= dangerousRange.Value)
+                LeviathanController.instance.tree.SetVariableValue("EnemyIsInDangerousRange", true);
+            else
+                LeviathanController.instance.tree.SetVariableValue("EnemyIsInDangerousRange", false);
+        }
     }
 }
+
