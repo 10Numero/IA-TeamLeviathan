a452b51ae6ebc471e8b382159db169fc8af40f64
10NUMERO <florian.gozard@edu.devinci.fr>
2021-10-14 18:56:40 +0200

    fix

--- Assets/Teams/Leviathan/CheckEnemyInShockwaveRange.cs
+++ Assets/Teams/Leviathan/CheckEnemyInShockwaveRange.cs
@@ -1,34 +1,17 @@
 using BehaviorDesigner.Runtime.Tasks;
 using BehaviorDesigner.Runtime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Leviathan;
 
 public class CheckEnemyInShockwaveRange : Action
 {
     public SharedFloat ShockwaveRange;
-    private LeviathanController leviathan;
-    private BehaviorTree tree;
 
     public override void OnStart()
     {
-        tree = gameObject.GetComponentInParent<BehaviorTree>();
-        leviathan = tree.GetComponentInParent<LeviathanController>();
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(leviathan.getSpaceship().Position, ShockwaveRange.Value, 10);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject.tag == "Player")
-            {
-                if (colliders[i].gameObject.GetComponent<LeviathanController>()._spaceship.Owner != leviathan._spaceship.Owner)
-                {
-                    tree.SetVariableValue("EnemyIsInShockwaveRange", true);
-                    i = colliders.Length;
-                }
-            }
-        }
+        float enemyDist = Vector2.Distance(LeviathanController.instance._spaceship.Position, LeviathanController.instance._otherSpaceship.Position);
 
+        if(enemyDist <= ShockwaveRange.Value)
+            LeviathanController.instance.tree.SetVariableValue("EnemyIsInShockwaveRange", true);
     }
-
-
 }
