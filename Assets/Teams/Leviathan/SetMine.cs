0940debe319a3e8ae66c89559f952725aa2113f1
Baptiste Barreyre <baptistebarreyre@hotmail.fr>
2021-10-14 19:19:17 +0200

    Dropping mine fixed

--- Assets/Teams/Leviathan/SetMine.cs
+++ Assets/Teams/Leviathan/SetMine.cs
@@ -7,22 +7,5 @@ using Leviathan;
 
 public class SetMine : Action
 {
-    public SharedBool mine;
-    private LeviathanController leviathan;
-    private BehaviorTree tree;
-
-    public override void OnStart()
-    {
-        tree = gameObject.GetComponentInParent<BehaviorTree>();
-        leviathan = tree.GetComponentInParent<LeviathanController>();
-        useMine(mine.Value);
-        
-    }
-
-    public void useMine(bool placeMine)
-    {
-        
-        leviathan.setMineCondition(placeMine);
-        tree.SetVariableValue("dropMine", placeMine);
-    }
+    public override void OnStart() => LeviathanController.instance.setMineCondition(true);
 }
