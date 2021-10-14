8230f6408d475a04c8a5a961ffa1721751661a93
Baptiste Barreyre <baptistebarreyre@hotmail.fr>
2021-10-14 19:03:02 +0200

    Fix dangerouszones

--- Assets/Teams/Leviathan/CheckEnemyInDangerousZone.cs
+++ Assets/Teams/Leviathan/CheckEnemyInDangerousZone.cs
@@ -8,26 +8,13 @@ using Leviathan;
 public class CheckEnemyInDangerousZone : Action
 {
     public SharedFloat dangerousRange;
-    private LeviathanController leviathan;
-    private BehaviorTree tree;
 
     public override void OnStart()
     {
-        tree = gameObject.GetComponentInParent<BehaviorTree>();
-        leviathan = tree.GetComponentInParent<LeviathanController>();
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(leviathan.getSpaceship().Position, dangerousRange.Value, 10);
-        for(int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject.tag == "Player")
-            {
-                if(colliders[i].gameObject.GetComponent<LeviathanController>()._spaceship.Owner != leviathan._spaceship.Owner){
-                    tree.SetVariableValue("EnemyIsInDangerousRange", true);
-                    i = colliders.Length;
-                }
-            }
-        }
-        
-    }
+        float enemyDist = Vector2.Distance(LeviathanController.instance._spaceship.Position, LeviathanController.instance._otherSpaceship.Position);
 
+        if(enemyDist <= dangerousRange.Value)
+            LeviathanController.instance.tree.SetVariableValue("EnemyIsInDangerousRange", true);
+    }
 
 }
